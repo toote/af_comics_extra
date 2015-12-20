08385e447516d2d3058f269886dcfe7d62dc682a
Matías A. Bellone <yo@toote.com.ar>
2015-12-20 02:57:53 -0300

    Note to self: concatenation in PHP is . and not +

--- af_comics_manly.php
+++ af_comics_manly.php
@@ -3,25 +3,24 @@ class Af_Comics_Manly extends Af_ComicFilter {
 	function supported() {
 		return array("Manly Guys Doing Manly Things");
 	}
-	
+
 	function process(&$article) {
 		if (strpos($article["link"], "thepunchlineismachismo.com/") !== FALSE) {
 			$doc = new DOMDocument();
 			@$doc->loadHTML(fetch_file_contents($article["link"]));
-			
+
 			$basenode = false;
-			
+
 			if ($doc) {
 				$xpath = new DOMXPath($doc);
 				$basenode = $xpath->query('(//div[@id="comic"]//img)')->item(0);
-				
 				if ($basenode) {
-					$article["content"] += $doc->saveXML($basenode);
+					$article["content"] .= $doc->saveXML($basenode);
 				}
 			}
 			return true;
 		}
-		
+
 		return false;
 	}
 }
--- af_comics_oglaf.php
+++ af_comics_oglaf.php
@@ -3,27 +3,27 @@ class Af_Comics_Oglaf extends Af_ComicFilter {
 	function supported() {
 		return array("Oglaf");
 	}
-	
+
 	function process(&$article) {
 		if (strpos($article["link"], "oglaf.com/") !== FALSE) {
 			$doc = new DOMDocument();
 			@$doc->loadHTML(fetch_file_contents($article["link"]));
-			
+
 			$basenode = false;
-			
+
 			if ($doc) {
 				$xpath = new DOMXPath($doc);
 				$basenode = $xpath->query('(//img[@id="strip"])')->item(0);
-				
+
 				if ($basenode) {
-					$article["content"] += $doc->saveXML($basenode);
+					$article["content"] .= $doc->saveXML($basenode);
 				}
 			}
 
             /* TODO: add ability to get multi-page stories */
 			return true;
 		}
-		
+
 		return false;
 	}
 }
